164b5fa3b9487a9f5509724fb9fd8998aaccbd5e
dannydreamer <daniel.prag@gmail.com>
2026-05-12 10:46:39 +0300

    Delete workshop-attendance directory

--- workshop-attendance/src/app/layout.js
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: 'נוכחות סדנאות', description: 'כלי לניהול נוכחות בסדנאות' };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="he" dir="rtl">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
