977c7b03a93d00d505ccd265d1fdc55682bc0f7f
dannydreamer <daniel.prag@gmail.com>
2026-05-12 10:53:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['docx']
-  }
-}
-
+const nextConfig = {}
 module.exports = nextConfig
